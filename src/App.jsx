182ebb01e98169615ed2b7eacfcb014a9a80716d
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-09 11:00:02 +0500

    SHARJEEL ahMED

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import Home from "./pages/home";
 import DonorListing from "./pages/donor";
 import Chat from "./pages/cat";
 import AdminDashboard from "./pages/admin";
-import Header from "./components/common/header/Header";
 import Request from "./pages/request";
 import Profile from "./pages/profile";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin"  element={<AdminDashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/register" element={"register"} />
